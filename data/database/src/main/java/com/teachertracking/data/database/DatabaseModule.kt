9fb127a9764065afc7d8d48d7b6bcc0280ad85ea
freez <sm.jalali.f@gmail.com>
2024-12-13 19:06:14 +0330

    add database module(hilt providing)

--- data/database/src/main/java/com/teachertracking/data/database/DatabaseModule.kt
+++ data/database/src/main/java/com/teachertracking/data/database/DatabaseModule.kt
@@ -1,5 +1,40 @@
 package com.teachertracking.data.database
 
-@modu
+import android.content.Context
+import androidx.room.Room
+import com.teachertracking.data.database.dao.ClubsDao
+import com.teachertracking.data.database.dao.EventDao
+import com.teachertracking.data.database.dao.StudentsDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 internal object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context): TeacherTrackingDatabase {
+        return Room.databaseBuilder(
+            context,
+            TeacherTrackingDatabase::class.java,
+            "teacher-tracking-db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideClubsDap(db: TeacherTrackingDatabase): ClubsDao = db.clubsDao()
+
+    @Provides
+    @Singleton
+    fun provideStudentsDap(db: TeacherTrackingDatabase): StudentsDao = db.studentsDao()
+
+    @Provides
+    @Singleton
+    fun provideEventsDap(db: TeacherTrackingDatabase): EventDao = db.eventsDao()
 }
